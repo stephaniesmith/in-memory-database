258ec07c726daf2d3e677d086f9d889c10ba4217
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-04 09:43:50 -0700

    test is check for new ids

--- test/store.test.js
+++ test/store.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const Store = require('../lib/store');
-const uniqid = require('uniqid');
+// const uniqid = require('uniqid');
 
 describe('store', () => {
     let newStore;
@@ -8,7 +8,7 @@ describe('store', () => {
         newStore = new Store();
     }); 
 
-    const objects = [
+    const podcasts = [
         {
             name: 'This American Life',
             host: 'Ira Glass'
@@ -23,22 +23,21 @@ describe('store', () => {
         }
     ]; 
 
-    it('returns new store with id', () => {
-        const objects = 'An array of podcasts';
-        const podcasts = newStore.save(objects);
-        assert.equal(podcasts, [
+    it('returns newStore with podcasts and adds _id to each podcast', () => {
+        const savedpodcasts = newStore.save(podcasts);
+        assert.deepEqual(savedpodcasts, [
             {
-                _id: 'some id',
+                _id: podcasts[0]._id,
                 name: 'This American Life',
                 host: 'Ira Glass'
             },
             {
-                _id: 'some id',
+                _id: podcasts[1]._id,
                 name: 'Welcome to Night Vale',
                 host: 'Cecil Palmer'
             },
             {
-                _id: 'some id',
+                _id: podcasts[2]._id,
                 name: 'She Explores',
                 host: 'Gale Straub'
             }
